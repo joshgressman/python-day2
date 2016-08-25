ac6173fae560f1c93ac8db2cdea3c16d57e8071e
joshgressman <joshgressman@gmail.com>
2016-08-24 19:56:20 -0500

    push did not register

---
+++ python_day2.js
@@ -0,0 +1,11 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 26 2016, 10:47:25)
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> age = input("Enter your name: ")
+Enter your name: 31
+>>> print("You have lived for {}".format(str(int(age) * 356 * 24 * 60 * 60)))
+You have lived for 953510400
+>>> #anoter way to use formats and inputs
